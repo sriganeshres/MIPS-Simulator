797507b5803ce5218f238e9115255914f7d29db2
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2024-10-07 11:36:28 +0530

    Fixed .data section parsing

--- input.asm
+++ input.asm
@@ -10,5 +10,8 @@ main:
     lw $t1, 0($t0)    # Load var2 into $t1  
     add $t2, $t0, $t1 # Add $t0 and $t1, store result in $t2  
     sub $t3, $t1, $t0 # Subtract $t0 from $t1, store result in $t3  
-    beq $t2, $t3, end # Branch to 'end' if $t2 equals $t3  
+    beq $t2, $t3, hello # Branch to 'end' if $t2 equals $t3  
     j main          # Jump back to the main label  
+
+hello:
+    j main
